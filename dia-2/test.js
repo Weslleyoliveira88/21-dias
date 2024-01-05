81b02451569d577876593c921dbfb97cbf6fc898
unknown <oliverweslley807@gmail.com>
2024-01-05 17:44:30 -0300

    calculando a idade eo IMC

--- dia-2/test.js
+++ dia-2/test.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+let Nome = prompt("Digite Seu Nome!!");
+let Idade = parseInt(prompt("Digite Sua Idade!!"));
+let Altura = parseInt(prompt("Digite sua Altura em centímetros!!"));
+let Peso = parseInt(prompt("Digite seu Peso em quilogramas!!"));
+
+let nasc = 2024 - Idade; 
+let IMC = Peso / ((Altura / 100) * (Altura / 100)); 
+
+console.log("Olá " + Nome + ", você tem " + Idade + " anos, nasceu em " + nasc + ", tem " + Altura + " cm de altura, pesa " + Peso + " kg e seu IMC é " + IMC.toFixed(2) + " Kg/m2");
